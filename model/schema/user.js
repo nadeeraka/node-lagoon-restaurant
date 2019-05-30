ccd65bbeb30abb02db956b1b573635bbde4778dd
nadeeraka <nimanthanadeeraka@outlook.com>
2019-05-30 14:19:27 +0530

    model added

---
+++ model/schema/user.js
@@ -0,0 +1,19 @@
+const db = require("../../db/connect");
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  user: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
+});
+
+module.exports = mongoose.model("user", userSchema);
--- src/app.js
+++ src/app.js
@@ -2,12 +2,22 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const db = require("../db/connect");
+const bodyParser = require("body-parser");
+const mainRoutes = require("../routes/basicRoues");
 
 // custom vars
 const PORT = 8000;
 
 // get permission access  public folder
 app.use(express.static(path.join(__dirname, "public")));
+// custom routes
+app.use(mainRoutes);
+// bodyParser
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// set ejs
+app.set("view engine", "ejs");
+app.set("views", "views");
 
 if (process.env.port) {
   app.listen(process.env.port, () => {
